aed5059d7acb12f188c5e4db40e0fc86885a21c4
oscarbrimelow <oscarbrimelow@gmail.com>
2025-12-25 20:06:43 +0200

    Replace 403 Forbidden audio links with working placeholders

--- src/audio/engine.js
+++ src/audio/engine.js
@@ -2,20 +2,20 @@ import { Howl, Howler } from 'howler'
 
 const sounds = {
   bgm: new Howl({
-    src: ['https://cdn.pixabay.com/download/audio/2022/03/24/audio_998c5b5eb6.mp3?filename=8-bit-rpg-120bpm-10636.mp3'],
+    src: ['https://assets.mixkit.co/active_storage/sfx/123/123-preview.mp3'], // Placeholder replacement
     loop: true,
     volume: 0.35
   }),
   blip: new Howl({
-    src: ['https://cdn.pixabay.com/download/audio/2021/09/29/audio_9b2f64f334.mp3?filename=ui-confirmation-1-1765.mp3'],
+    src: ['https://assets.mixkit.co/active_storage/sfx/2578/2578-preview.mp3'], // Placeholder replacement
     volume: 0.5
   }),
   collect: new Howl({
-    src: ['https://cdn.pixabay.com/download/audio/2022/03/15/audio_d4e8823717.mp3?filename=bonus-6160.mp3'],
+    src: ['https://assets.mixkit.co/active_storage/sfx/2000/2000-preview.mp3'], // Placeholder replacement
     volume: 0.6
   }),
   splash: new Howl({
-    src: ['https://cdn.pixabay.com/download/audio/2022/03/10/audio_bb2a61d4d0.mp3?filename=water-splash-5956.mp3'],
+    src: ['https://assets.mixkit.co/active_storage/sfx/2003/2003-preview.mp3'], // Placeholder replacement
     volume: 0.6
   })
 }
